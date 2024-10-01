16cf84e01a65640a922c7e78d4ee6a3fea4fb0c6
Rashid Rao <rashid047571@gmail.com>
2024-10-01 17:44:20 +0200

    'updated_codes'

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
 from setuptools import setup, find_packages
 
-setup(name='lime_stratified',
-      version='0.1',
-      description='Using Stratified Sampling to Improve LIME Image Explanations',
-      url='https://github.com/rashidrao-pk/lime_stratified',
-      author='Muhammad Rashid',
-      author_email='rashidunito@gmail.com',
-      license='MIT',
-      packages=find_packages(exclude=['js', 'node_modules', 'tests']),
-      python_requires='>=3.5',
-      install_requires=[
-          'matplotlib',
-          'numpy',
-          'scipy',
-          'tqdm >= 4.29.1',
-          'scikit-learn>=0.18',
-          'scikit-image>=0.12',
-          'pyDOE2==1.3.0'
-      ],
-      extras_require={
-          'dev': ['pytest', 'flake8'],
-      },
-      include_package_data=True,
-      zip_safe=False)
+setup(
+    name='lime_stratified',
+    version='0.1',
+    description='Using Stratified Sampling to Improve LIME Image Explanations',
+    url='https://github.com/rashidrao-pk/lime_stratified',
+    author='Muhammad Rashid',
+    author_email='rashidunito@gmail.com',
+    license='MIT',
+    packages=find_packages(exclude=['js', 'node_modules']),
+    python_requires='>=3.5',
+    install_requires=[
+        'matplotlib>=3.0,<4.0',
+        'numpy>=1.16,<2.0',
+        'scipy>=1.2,<2.0',
+        'tqdm>=4.29.1,<5.0',
+        'scikit-learn>=0.18,<1.0',
+        'scikit-image>=0.12,<1.0',
+        'pyDOE2==1.3.0'
+    ],
+    extras_require={
+        'dev': ['pytest', 'flake8', 'black', 'mypy'],
+    },
+    include_package_data=True,
+    zip_safe=False
+)
